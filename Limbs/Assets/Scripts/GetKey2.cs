3b25a2ebe68a82bd2bf9b399c298b281425eb247
nmul1801 <nicholas.m.mulligan@gmail.com>
2022-12-04 12:10:00 -0500

    started tile mapping

--- Limbs/Assets/Scripts/GetKey2.cs
+++ Limbs/Assets/Scripts/GetKey2.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class GetKey2 : MonoBehaviour
 {
+    private GameController gc;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // get the game controller object
+        gc = GameObject.Find("GameController").GetComponent<GameController>();
+        // show eyes if they are set to be shown
+        door.SetActive(gc.ShowDoor1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OnTriggerEnter2D (Collider2D other) {
+        if (other.gameObject.tag == "Player"){
+            Destroy(gameObject);
+            gc.SwitchSceneAfterEyesPickup();
+        }
     }
 }
