61c33c2d7a5dad9951ced74af8c578e3cb70059e
ArundeepSingh <Arundeep.Singh@tufts.edu>
2022-12-11 22:58:38 -0500

    Added movement script for final boss

---
+++ Limbs/Assets/Scripts/LegsPickup.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LegsPickup : MonoBehaviour
+{
+    private GameController gc;
+    // public GameObject door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get the game controller object
+        gc = GameObject.Find("GameController").GetComponent<GameController>();
+        // show eyes if they are set to be shown
+        gameObject.SetActive(gc.ShowEyes);
+        // door.SetActive(gc.ShowDoor1);
+    }
+
+    public void OnTriggerEnter2D (Collider2D other) {
+        if (other.gameObject.tag == "Player"){
+            Destroy(gameObject);
+            // gc.SwitchSceneAfterEyesPickup();
+            SceneManager.LoadScene("BossScene");
+        }
+    }
+}
