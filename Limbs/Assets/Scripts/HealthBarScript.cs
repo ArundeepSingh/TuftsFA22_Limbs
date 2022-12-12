20caf35d138fc1a838395d43d182376c524d555b
ArundeepSingh <Arundeep.Singh@tufts.edu>
2022-12-12 14:41:08 -0500

    Added transition to losescene upon death

--- Limbs/Assets/Scripts/HealthBarScript.cs
+++ Limbs/Assets/Scripts/HealthBarScript.cs
@@ -61,6 +61,6 @@ public class HealthBarScript : MonoBehaviour {
            // death stuff. change scene? how about a particle effect?
             //Vector3 objPos = this.transform.position
             //Instantiate(deathEffect, objPos, Quaternion.identity) as GameObject;
-           //SceneManager.LoadScene ("Scene_lose");
+           SceneManager.LoadScene("LoseScene");
       }
 }
