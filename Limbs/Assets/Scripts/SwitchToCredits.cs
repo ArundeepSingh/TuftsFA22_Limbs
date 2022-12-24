8540b77222665586c3663119fa7cf1d1374c8295
nmul1801 <nicholas.m.mulligan@gmail.com>
2022-12-23 21:14:16 -0600

    resolved

--- Limbs/Assets/Scripts/SwitchToCredits.cs
+++ Limbs/Assets/Scripts/SwitchToCredits.cs
@@ -9,7 +9,6 @@ public class SwitchToCredits : MonoBehaviour
     private MonoBehaviour CameraFollowscript;
 
     void Start() {
-        CameraFollowscript = Camera.main.GetComponent("CameraFollow2DLERP") as MonoBehaviour;
         CameraFollowscript.enabled = false;
     }
 
